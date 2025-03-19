b13a977e081cb1fdf162fd19976be1867329f484
unknownjoao <joaolucasfarias0203@gmail.com>
2025-03-19 02:02:15 -0400

    feat: adiciona componente AboutUs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
+import { AboutUs } from "@/components/AboutUs";
 import { HeroBanner } from "@/components/HeroBanner";
 import { Services } from "@/components/Services";
 
 export default function Home() {
   return (
     <>
-      <HeroBanner/>
-      <Services/>
+      <HeroBanner />
+      <Services />
+      <AboutUs />
     </>
   );
 }
