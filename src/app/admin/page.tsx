145a950ef17e16b0cbd40222d28fddb4d4060a1d
João Lucas <joaolucasfarias0203@gmail.com>
2025-05-23 15:13:17 -0300

    refactor: remover estilos de borda ao passar o mouse desnecessários das abas de navegação.

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -125,10 +125,10 @@ export default function AdminDashboard() {
               <nav className="-mb-px flex space-x-8">
                 <button
                   onClick={() => setActiveTab("appointments")}
-                  className={`py-4 px-1 border-b-2 font-medium cursor-pointer${
+                  className={`py-4 px-1 border-b-2 font-medium cursor-pointer ${
                     activeTab === "appointments"
                       ? "border-primary-blue text-primary-blue font-bold text-base"
-                      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 text-sm "
+                      : "border-transparent text-gray-500 hover:text-gray-700 text-sm"
                   }`}
                 >
                   <FontAwesomeIcon icon={faCalendarCheck} className="mr-2" />
@@ -139,7 +139,7 @@ export default function AdminDashboard() {
                   className={`py-4 px-1 border-b-2 font-medium cursor-pointer ${
                     activeTab === "reports"
                       ? "border-primary-blue text-primary-blue font-bold text-base"
-                      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 text-sm"
+                      : "border-transparent text-gray-500 hover:text-gray-700 text-sm"
                   }`}
                 >
                   <FontAwesomeIcon icon={faChartBar} className="mr-2" />
@@ -150,7 +150,7 @@ export default function AdminDashboard() {
                   className={`py-4 px-1 border-b-2 font-medium cursor-pointer ${
                     activeTab === "messages"
                       ? "border-primary-blue text-primary-blue font-bold text-base"
-                      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 text-sm"
+                      : "border-transparent text-gray-500 hover:text-gray-700 text-sm"
                   }`}
                 >
                   <FontAwesomeIcon icon={faMessage} className="mr-2" />
